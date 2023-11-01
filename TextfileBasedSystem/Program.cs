7a2362b240324c73eaa5d135068b2aadf039200c
abhaysurya10 <abhaysurya10@gmail.com>
2023-11-01 15:30:42 +0530

    Add project files.

---
+++ TextfileBasedSystem/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextfileBasedSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Teacher> teachers = TeacherManagement.LoadTeacherData();
+            while (true)
+            {
+                Console.WriteLine("Welcome to Rainbow School Teacher Data System");
+                Console.WriteLine("1. Display All Teachers");
+                Console.WriteLine("2. Add New Teacher");
+                Console.WriteLine("3. Update Teacher Data");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("Enter your Choice");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        TeacherManagement.DisplayAllTeachers(teachers);
+                        break;
+                    case 2:
+                        TeacherManagement.AddNewTeacher(teachers);
+                        break;
+                    case 3:
+                        TeacherManagement.UpdateTeacherData(teachers);
+                        break;
+                    case 4:
+                        TeacherManagement.SaveTeacherData(teachers);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+                Console.Write("Do you want to continue (yes/no)?:");
+                string continueChoice = Console.ReadLine();
+                if (continueChoice.ToLower() != "yes")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ TextfileBasedSystem/TeacherManagement.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextfileBasedSystem
+{
+    public class TeacherManagement
+    {
+        private const string FilePath = @"C:\Users\ADMIN\source\repos\TextfileBasedSystem\TextfileBasedSystem\TeacherData.txt";
+
+        public static void SaveTeacherData(List<Teacher> teachers)
+        {
+            using (StreamWriter writer = new StreamWriter(FilePath))
+            {
+                foreach (Teacher teacher in teachers)
+                {
+                    writer.WriteLine($"{teacher.ID},{teacher.Name},{teacher.Class},{teacher.Section}");
+                }
+            }
+        }
+
+        public static List<Teacher> LoadTeacherData()
+        {
+            List<Teacher> teachers = new List<Teacher>();
+
+            if (File.Exists(FilePath))
+            {
+                using (StreamReader reader = new StreamReader(FilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        if (parts.Length == 4)
+                        {
+                            int id = int.Parse(parts[0]);
+                            string name = parts[1];
+                            string teacherClass = parts[2];
+                            string section = parts[3];
+
+                            teachers.Add(new Teacher
+                            {
+                                ID = id,
+                                Name = name,
+                                Class = teacherClass,
+                                Section = section
+                            });
+                        }
+                    }
+                }
+            }
+
+            return teachers;
+        }
+
+
+        public static void UpdateTeacherData(List<Teacher> teachers)
+        {
+            Console.Write("\nEnter Teacher ID to update: ");
+            int idToUpdate = int.Parse(Console.ReadLine());
+
+            Teacher teacherToUpdate = teachers.Find(t => t.ID == idToUpdate);
+            if (teacherToUpdate == null)
+            {
+                Console.WriteLine("Teacher with specified ID not found.");
+                return;
+            }
+
+            Console.Write("Enter updated Teacher Name: ");
+            teacherToUpdate.Name = Console.ReadLine();
+
+            Console.Write("Enter updated Class: ");
+            teacherToUpdate.Class = Console.ReadLine();
+
+            Console.Write("Enter updated Section: ");
+            teacherToUpdate.Section = Console.ReadLine();
+
+            Console.WriteLine("Teacher data updated successfully!");
+        }
+
+
+        public static void DisplayAllTeachers(List<Teacher> teachers)
+        {
+            Console.WriteLine("\nAll Teachers:");
+            foreach (Teacher teacher in teachers)
+            {
+                Console.WriteLine($"ID: {teacher.ID}, Name: {teacher.Name}, Class: {teacher.Class}, Section: {teacher.Section}");
+            }
+        }
+
+        public static void AddNewTeacher(List<Teacher> teachers)
+        {
+            Console.Write("\nEnter Teacher ID: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Teacher Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Class: ");
+            string teacherClass = Console.ReadLine();
+
+            Console.Write("Enter Section: ");
+            string section = Console.ReadLine();
+
+            teachers.Add(new Teacher
+            {
+                ID = id,
+                Name = name,
+                Class = teacherClass,
+                Section = section
+            });
+
+            Console.WriteLine("Teacher data added successfully!");
+        }
+    }
+}
